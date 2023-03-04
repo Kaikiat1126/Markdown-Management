480cf386b04b9f1f1ac7fda1cdd4e80bbc547c86
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2023-03-04 16:53:57 +0800

    feat: init Note Page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import NewNote from './NewNote'
 import NoteList from './NoteList'
 import useLocalStorage from './useLocalStorage'
 import { v4 as uuidV4 } from 'uuid'
+import { NoteLayout } from './NoteLayout'
+import Note from './Note'
 
 export type Note = {
   id: string
@@ -59,8 +61,8 @@ function App() {
       <Routes>
         <Route path="/" element={<NoteList notes={notesWithTags} availableTags={tags}/>} />
         <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags}/>} />
-        <Route path="/:id">
-          <Route index element={<h1>Show</h1>}/>
+        <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
+          <Route index element={<Note />}/>
           <Route path='edit' element={<h1>Edit</h1>}/>
         </Route>
         <Route path="*" element={<Navigate to='/'/>} />
