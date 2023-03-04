9ff4e043758c397856e5eaaeecba8c08040c8d57
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2023-03-04 17:47:50 +0800

    feat: delete Note

--- src/App.tsx
+++ src/App.tsx
@@ -63,6 +63,12 @@ function App() {
     })
   }
 
+  function onDeleteNote(id: string){
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
+
   function addTag(tag: Tag) {
     console.log(tag);
     setTags(prev => [...prev, tag])
@@ -74,7 +80,7 @@ function App() {
         <Route path="/" element={<NoteList notes={notesWithTags} availableTags={tags}/>} />
         <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags}/>} />
         <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
-          <Route index element={<Note />}/>
+          <Route index element={<Note onDelete={onDeleteNote}/>}/>
           <Route path='edit' element={<EditNote onSubmit={onUpdateNote} onAddTag={addTag} availableTags={tags}/>}/>
         </Route>
         <Route path="*" element={<Navigate to='/'/>} />
